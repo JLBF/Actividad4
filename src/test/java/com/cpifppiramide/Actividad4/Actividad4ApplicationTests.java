113486c7bc6f976c22e71fdddbff0318227d0b39
Jose Luis Ballarin <19260@fppiramide.com>
2023-10-31 13:08:46 +0100

    commit incial

---
+++ src/test/java/com/cpifppiramide/Actividad4/Actividad4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpifppiramide.Actividad4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actividad4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
