70ba572e92e1a1d5ca75cea91ff0e8f712c04ffc
Jose Luis <19260@fppiramide.com>
2023-11-12 23:08:46 +0100

    Plantilla test

---
+++ src/test/java/com/cpifppiramide/Actividad4/peliculas/application/PeliculaUseCasesTest.java
@@ -0,0 +1,24 @@
+package com.cpifppiramide.Actividad4.peliculas.application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PeliculaUseCasesTest {
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
---
+++ src/test/java/com/cpifppiramide/Actividad4/usuarios/application/UsuarioUseCasesTest.java
@@ -0,0 +1,24 @@
+package com.cpifppiramide.Actividad4.usuarios.application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioUseCasesTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
